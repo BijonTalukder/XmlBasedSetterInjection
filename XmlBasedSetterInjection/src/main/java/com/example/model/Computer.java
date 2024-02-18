811092ae3b9af6762296f7dfb0c7c4d3b2bc82d1
BijonTalukder <bijontalukder1247@gmail.com>
2024-02-18 12:07:10 +0600

    a

---
+++ XmlBasedSetterInjection/src/main/java/com/example/model/Computer.java
@@ -0,0 +1,16 @@
+package com.example.model;
+
+public class Computer {
+	private String brand;
+	public Computer() {
+		super();
+	}
+	public String getBrand() {
+		return brand;
+	}
+	public void setBrand(String brand) {
+		this.brand = brand;
+		
+	}
+
+}
